fc438b34e35c4334a19c60e3faa54f012eaffa64
Himanshu Singh <himanshuk282@gmail.com>
2024-04-06 14:10:13 +0530

    comparision in js

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" <= 1);
+// console.log("02" <= 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 1);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// // === : strict equality check; checks the value as well as type of the operands
+
+// console.log("2" === 2);
