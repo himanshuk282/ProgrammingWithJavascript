d521b64d7829f3537815e56bae4e8a2a3a74c0e4
Himanshu Singh <himanshuk282@gmail.com>
2024-04-06 13:51:58 +0530

    dataTypes in js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Himanshu")
+
+
+let name = "Himanshu"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 => Handles double,float,positive and negative numbers
+// bigint => Rarely used but when you can not store it in number you can store it in bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //returns undefined
+console.log(typeof null); //returns object
